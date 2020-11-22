479b2bf9a7d3853f18f59e128bcc185a162be7e3
mdj20130907 <mdj20130907@gmail.com>
2020-11-22 13:10:48 +0800

    一直读取，不断开连接

--- demo/raspberrypi/test004_socket/socket_client.py
+++ demo/raspberrypi/test004_socket/socket_client.py
@@ -10,9 +10,14 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect(server_address)
 
 try:
-    message = 'client message'
-    sock.sendall(message.encode())
-    reply = sock.recv(1024)
-    print(reply.decode())
+    while True:
+        command = input('Please input command name, exit/blank to stop:')
+        print('input: ' + command)
+        if not command or 'exit' == command:
+            # 断开连接，不再发送数据
+            break
+        elif command in ('stop', 'forward', 'back', 'left', 'right'):
+            print('send: ' + command)
+            sock.sendall(command.encode())
 finally:
     sock.close()
--- demo/raspberrypi/test004_socket/socket_server.py
+++ demo/raspberrypi/test004_socket/socket_server.py
@@ -3,6 +3,8 @@
 import socket
 from threading import Thread
 
+# TODO: 队列保证了在一个时间点只有一个command在执行
+
 server_address = ('127.0.0.1', 9527, )
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -12,19 +14,24 @@ sock.bind(server_address)
 sock.listen(1)
 print('listening on %s:%s' % server_address)
 
-def excute(command):
-    print(command)
-    if command in ('stop', 'forward', 'back', 'left', 'right'):
-        # TODO:
-        pass
+def execute(command):
+    # TODO:
+    pass
 
 while True:
     print('waiting for a connection')
     (connection, client_address, ) = sock.accept()
     print('connection from %s:%s' % client_address)
     try:
-        command = connection.recv(1024).decode()
-        Thread(target=excute, args=(command, )).start()
+        while True:
+            command = connection.recv(1024).decode()
+            print('receive: ' + command)
+            if not command or 'exit' == command:
+                # 客户端已经断开连接/发送空数据，不再继续读取数据
+                break
+            elif command in ('stop', 'forward', 'back', 'left', 'right'):
+                print('execute: ' + command)
+                Thread(target=execute, args=(command, )).start()
     finally:
         connection.close()
 
