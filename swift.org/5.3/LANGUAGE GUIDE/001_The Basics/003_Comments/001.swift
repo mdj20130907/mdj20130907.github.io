94c181c979134acbf7981feaf85ef7e57fa2d94f
mdj20130907 <mdj20130907@gmail.com>
2020-12-18 16:15:58 +0800

    注释，分号

---
+++ swift.org/5.3/LANGUAGE GUIDE/001_The Basics/003_Comments/001.swift
@@ -0,0 +1,14 @@
+// Single-line comments begin with two forward-slashes (//):
+
+// Multiline comments start with a forward-slash followed by an asterisk (/*) and,
+// end with an asterisk followed by a forward-slash (*/):
+print("hello"/*打印*/)
+
+// multiline comments in Swift can be nested inside other multiline comments
+
+/* 外层注释
+    /* 内层注释 */
+这里也是注释*/
+
+// Nested multiline comments enable you to comment out large blocks of code quickly and easily,
+// even if the code already contains multiline comments.
---
+++ swift.org/5.3/LANGUAGE GUIDE/001_The Basics/004_Semicolons/001.swift
@@ -0,0 +1,6 @@
+// Unlike many other languages, Swift doesn’t require you to write a semicolon (;) after each statement in your code,
+// although you can do so if you wish.
+// However, semicolons are required if you want to write multiple separate statements on a single line:
+
+let cat = "🐱"; print(cat)
+
