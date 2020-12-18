2153f1b95c7e3c074e247818df563a6b16061403
mdj20130907 <mdj20130907@gmail.com>
2020-12-18 15:56:35 +0800

    floating-point numbers

---
+++ swift.org/5.3/LANGUAGE GUIDE/001_The Basics/006_Floating-Point Numbers/001.swift
@@ -0,0 +1,7 @@
+// Double represents a 64-bit floating-point number.
+// Float represents a 32-bit floating-point number.
+
+// Double has a precision of at least 15 decimal digits,
+// whereas the precision of Float can be as little as 6 decimal digits.
+// In situations where either type would be appropriate, `Double` is preferred.
+
