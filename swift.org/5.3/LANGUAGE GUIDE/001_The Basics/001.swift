576e1008e2bdbb55cdbcc90531fd9984bc917328
mdj20130907 <mdj20130907@gmail.com>
2020-12-18 16:26:45 +0800

    variables, constants

---
+++ swift.org/5.3/LANGUAGE GUIDE/001_The Basics/001.swift
@@ -0,0 +1,9 @@
+// Int integers
+// Double/Float floating-point values
+// Bool Boolean values
+// String textual data
+
+// Array, Set, Dictionary
+
+// tuples
+// optional types
---
+++ swift.org/5.3/LANGUAGE GUIDE/001_The Basics/002_Constants and Variables/001.swift
@@ -0,0 +1,18 @@
+// You can declare multiple constants or multiple variables on a single line, separated by commas:
+var x = 0.0, y = 0.0, z = 0.0
+
+// If a stored value in your code won’t change, always declare it as a constant with the let keyword.
+// Use variables only for storing values that need to be able to change.
+
+// type annotation
+// The colon in the declaration means “…of type…”
+var welcomeMessage: String
+welcomeMessage = "hello"
+
+// You can define multiple related variables of the same type on a single line,
+// separated by commas, with a single type annotation after the final variable name
+var red, green, blue: Double
+
+// It’s rare that you need to write type annotations in practice.
+// infer the type
+
