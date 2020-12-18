73231c6eec47755f7d0809ab2b1b8f65d485e666
mdj20130907 <mdj20130907@gmail.com>
2020-12-18 17:44:58 +0800

    logical

---
+++ swift.org/5.3/LANGUAGE GUIDE/002_Basic Operators/010_Logical Operators.swift
@@ -0,0 +1,17 @@
+// Logical NOT (!a)
+// is a prefix operator,
+// and appears immediately before the value it operates on, without any white space
+
+// Logical AND (a && b)
+// short-circuit evaluation
+
+// Logical OR (a || b)
+// short-circuit evaluation
+
+// Combining Logical Operators
+// The Swift logical operators && and || are left-associative,
+// meaning that compound expressions with multiple logical operators evaluate the leftmost subexpression first.
+
+// Explicit Parentheses
+// Readability is always preferred over brevity;
+// use parentheses where they help to make your intentions clear.
