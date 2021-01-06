5e493b774736b5f63a62da34b2b77bdc158bccf4
mdj20130907 <mdj20130907@gmail.com>
2021-01-06 17:43:20 +0800

    jdk+ccache+python

--- demo/docker/006_docker_aosp/docker_build.sh
+++ demo/docker/006_docker_aosp/docker_build.sh
@@ -1,3 +1,6 @@
+# 浏览器下载jdk8,放在该目录: jdk8-master.tar.gz
+# https://android.googlesource.com/platform/prebuilts/jdk/jdk8/+archive/master.tar.gz
+
 docker build \
 --build-arg userid=$(id -u) \
 --build-arg groupid=$(id -g) \
